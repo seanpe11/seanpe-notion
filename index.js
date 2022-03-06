34325dc2f4d99f28d7c1cfdbf95c8ebe197ca262
seanpe11 <sean.m.s.pe@gmail.com>
2022-03-06 17:10:54 +0800

    added cors

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const { Client } = require("@notionhq/client")
 const app = express()
 const port = process.env.PORT || 3000
+const cors = require('cors')
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
